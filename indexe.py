8fb47c232d1a96d7aecd606a43b977e954e48a83
chris <christronaldo025@gmail.com>
2026-01-08 22:10:35 +0530

    training

--- indexe.py
+++ indexe.py
@@ -2,3 +2,8 @@ print('learn git')
 print('this is a message from new branch')
 print('this is from demo branch')
 print('onemore demo')
+i = 1
+if i>10:
+    print('i is grater')
+else:
+    print('i is lessthan')
